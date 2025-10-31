04cf8fa370ad8d04379c43f657409baa4a8b53f4
clementtt1 <clem50.schneider@gmail.com>
2025-10-31 15:23:16 +0100

    clubController fixed

--- src/Controller/ClubController.php
+++ src/Controller/ClubController.php
@@ -8,11 +8,25 @@ use Symfony\Component\Routing\Attribute\Route;
 
 final class ClubController extends AbstractController
 {
-    #[Route('/club', name: 'app_club')]
-    public function index(): Response
-    {
-        return $this->render('club/index.html.twig', [
-            'controller_name' => 'ClubController',
-        ]);
+    #[Route('/', name: 'club_map')]
+    public function index(ClubRepository $clubRepository): Response
+        {
+            $clubs = $clubRepository->findAll();
+
+    // Transformer en tableau simple
+    $clubsArray = array_map(function($club) {
+        return [
+            'name' => $club->getName(),
+            'stadium' => $club->getStadium(),
+            'latitude' => $club->getLatitude(),
+            'longitude' => $club->getLongitude(),
+            'logo' => '/images/logos/' . $club->getLogo(), // chemin relatif vers le logo
+        ];
+    }, $clubs);
+
+    return $this->render('club/index.html.twig', [
+        'clubs' => $clubsArray,
+    ]);
+
     }
 }
