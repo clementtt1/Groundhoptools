cdaabc9d421fa73b056bd92eb38ad2cb5c37ec18
clementtt1 <clem50.schneider@gmail.com>
2025-11-02 11:30:48 +0100

    Users -> User

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -2,11 +2,11 @@
 
 namespace App\Entity;
 
-use App\Repository\UsersRepository;
+use App\Repository\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
 
-#[ORM\Entity(repositoryClass: UsersRepository::class)]
-class Users
+#[ORM\Entity(repositoryClass: UserRepository::class)]
+class User
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
