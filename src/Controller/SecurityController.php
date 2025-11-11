9d44543f4e86430048416a0c6f30c0d7ab5b6e80
clementtt1 <clem50.schneider@gmail.com>
2025-11-11 19:11:28 +0100

    messages d'erreur et de succès

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -32,32 +32,48 @@ class SecurityController extends AbstractController
 
 
     #[Route('/register', name: 'app_register', methods: ['POST'])]
-    public function register(
-        Request $request,
-        EntityManagerInterface $em,
-        UserPasswordHasherInterface $hasher
-    ): Response {
-        $username = $request->request->get('username');
-        $email = $request->request->get('email');
-        $password = $request->request->get('password');
+public function register(
+    Request $request,
+    EntityManagerInterface $em,
+    UserPasswordHasherInterface $hasher
+): Response {
+    $username = $request->request->get('username');
+    $email = $request->request->get('email');
+    $password = $request->request->get('password');
 
-        if ($username && $email && $password) {
-            $user = new User();
-            $user->setUsername($username);
-            $user->setEmail($email);
-            $user->setPassword($hasher->hashPassword($user, $password));
-            $user->setIsActive(true);
-
-            $em->persist($user);
-            $em->flush();
-
-            $this->addFlash('success', 'Votre compte a bien été créé. Vous pouvez vous connecter !');
-            return $this->redirectToRoute('app_login');
-        }
-
+    if (!$username || !$email || !$password) {
         $this->addFlash('error', 'Tous les champs sont obligatoires.');
         return $this->redirectToRoute('app_login');
     }
+
+    // Vérifier si le username existe déjà
+    $existingUserByUsername = $em->getRepository(User::class)->findOneBy(['username' => $username]);
+    if ($existingUserByUsername) {
+        $this->addFlash('error', 'Ce nom d’utilisateur est déjà utilisé.');
+        return $this->redirectToRoute('app_login');
+    }
+
+    // Vérifier si l'email existe déjà
+    $existingUserByEmail = $em->getRepository(User::class)->findOneBy(['email' => $email]);
+    if ($existingUserByEmail) {
+        $this->addFlash('error', 'Cet email est déjà utilisé.');
+        return $this->redirectToRoute('app_login');
+    }
+
+    // Création du nouvel utilisateur
+    $user = new User();
+    $user->setUsername($username);
+    $user->setEmail($email);
+    $user->setPassword($hasher->hashPassword($user, $password));
+    $user->setIsActive(true);
+
+    $em->persist($user);
+    $em->flush();
+
+    $this->addFlash('success', 'Votre compte a bien été créé. Vous pouvez vous connecter !');
+    return $this->redirectToRoute('app_login');
+}
+
 
     #[Route(path: '/logout', name: 'app_logout')]
     public function logout(): void
