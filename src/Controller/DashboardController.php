862c12dae4386ec8a879375b7295784bee646828
clementtt1 <clem50.schneider@gmail.com>
2025-11-11 23:36:16 +0100

    personal map

---
+++ src/Controller/DashboardController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DashboardController extends AbstractController
+{
+    #[Route('/dashboard', name: 'app_dashboard')]
+    public function index(): Response
+    {
+        $user = $this->getUser();
+
+        if (!$user) {
+            return $this->redirectToRoute('app_login');
+        }
+
+        $visitedClubs = $user->getStadiumsVisited()->map(function($club) {
+            return [
+                'id' => $club->getId(),
+                'name' => $club->getNomClub(),
+                'stadium' => $club->getNomStadeClub(),
+                'latitude' => $club->getLatitudeStadeClub(),
+                'longitude' => $club->getLongitudeStadeClub(),
+                'logo' => $club->getLogoClub(),
+            ];
+        })->toArray();
+
+        return $this->render('dashboard/index.html.twig', [
+            'clubs' => $visitedClubs,
+        ]);
+    }
+}
